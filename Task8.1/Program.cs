ded087411c95be03fb5fc29e19de526abb8de377
user <user@WIN-8I41D9CN0BC>
2022-12-02 00:13:28 +0300

    Добавьте файлы проекта.

---
+++ Task8.1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Task8._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"E:\ES";
+            string pattern = ".";
+            string[] files = Directory.GetDirectories(path, pattern, SearchOption.AllDirectories);
+            foreach (string file in files)
+            {
+                Console.WriteLine(file);
+            }
+                Console.ReadKey();
+        } 
+    }
+}
---
+++ Task8.2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Task8._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"E:\ES\Test.txt";
+            Random random = new Random();
+            using (StreamWriter sw = new StreamWriter(path, false))
+            {
+                for (int i=0; i<10; i++)
+                {
+                    sw.WriteLine(random.Next(-10, 10));
+                }
+            }
+            int S = 0;
+            using (StreamReader sr = new StreamReader(path))
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                   S+=Convert.ToInt32(sr.ReadLine());
+                    
+                }
+                Console.WriteLine(S);
+                Console.ReadKey();
+
+            }
+        }
+    }
+}
